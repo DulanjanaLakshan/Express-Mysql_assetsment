eceb1cb9d0b8e23e31640434a9c4d3b1f70b1559
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-08-29 14:39:42 +0530

    update

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express=require('express');
 const app=express();
 const port = 4000;
 const customer=require('./router/customer');
+const item=require('./router/Items');
+const user=require('./router/user');
 
 
 app.use(express.json())
 
-// app.use('/customer',customer)
+app.use('/customer',customer)
+app.use('/item',item)
+app.use('/user',user)
 
 
 app.listen(port, () => {
--- router/user.js
+++ router/user.js
@@ -8,7 +8,7 @@ connection.connect((error) => {
     if (error) {
         console.log(error);
     } else {
-        var customertable = "CREATE TABLE IF NOT EXISTS item(id varchar(255) PRIMARY KEY,name varchar(255), description varchar(255), qty varchar(255), price varchar(255))"
+        var customertable = "CREATE TABLE IF NOT EXISTS user(id varchar(255) PRIMARY KEY,name varchar(255), address varchar(255), age varchar(255), phone varchar(255))"
         connection.query(customertable, (error, result) => {
             console.log("Connect database");
         })
@@ -16,10 +16,10 @@ connection.connect((error) => {
 })
 
 router.get('/', (req, res) => {
-    const query = "SELECT * FROM item"
+    const query = "SELECT * FROM user"
     connection.query(query, (error, row) => {
         if (error) {
-            res.send("No Item")
+            res.send("No User")
         } else {
             res.send(row)
         }
@@ -29,30 +29,30 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const id = req.body.id;
     const name = req.body.name;
-    const description = req.body.email;
-    const qty = req.body.city;
-    const price = req.body.number;
+    const address = req.body.email;
+    const age = req.body.city;
+    const phone = req.body.number;
 
-    const query = "INSERT INTO item (id,name,description,qty,price) values (?,?,?,?,?)"
-    connection.query(query, [id, name, description, qty, price], (error) => {
+    const query = "INSERT INTO user (id,name,address,age,phone) values (?,?,?,?,?)"
+    connection.query(query, [id, name, address, age, phone], (error) => {
         if (error) {
-            res.send("Item Alrady Saved..!")
+            res.send("User Alrady Saved..!")
         } else {
-            res.send("Item Save Sucsess..!")
+            res.send("User Save Sucsess..!")
         }
     })
     console.log(req.body);
 })
 
 router.put('/', (req, res) => {
-    const id = req.body.id;
+    const id = req.body.id;  
     const name = req.body.name;
-    const description = req.body.email;
-    const qty = req.body.city;
-    const price = req.body.number;
+    const address = req.body.email;
+    const age = req.body.city;
+    const phone = req.body.number;
 
-    var query = "UPDATE item SET name=?, description=?, qty=?, price=? WHERE id=?";
-    connection.query(query, [name, description, qty, price, id], (error, row) => {
+    var query = "UPDATE user SET name=?, address=?, age=?, phone=? WHERE id=?";
+    connection.query(query, [name, address, age, phone, id], (error, row) => {
         if (error) throw error;
         res.send(row)
     })
@@ -60,12 +60,12 @@ router.put('/', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     const id = req.params.id
-    var query = "DELETE FROM item WHERE id=?";
+    var query = "DELETE FROM user WHERE id=?";
     connection.query(query, [id], (error, row) => {
         if (error) {
-            res.send("Item Not Found")
+            res.send("user Not Found")
         } else {
-            res.send("Item Delete")
+            res.send("user Delete")
             res.send(row)
         }
     })
@@ -73,11 +73,11 @@ router.delete('/:id', (req, res) => {
 
 router.get('/:id',(res,req)=>{
     const id=req.params.id
-    var query="SELECT * FROM item WHERE id=?";
+    var query="SELECT * FROM user WHERE id=?";
 
     connection.query(query[id],(error,row)=>{
         if(error){
-            res.send("Item Not Found")
+            res.send("user Not Found")
         }else{
             res.send(row)
         }
